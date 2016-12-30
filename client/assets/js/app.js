12cc450eee548246ab2d06138f2f1d50aa5f4d1c
Smerth <merth@me.com>
2016-12-30 14:05:03 -0800

    removed extra .config()

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -81,12 +81,12 @@
 
 
 // This attempt to fix CORS issue on Safari and Firefox doesn't work...
-    .config(function($httpProvider) {
-         //Enable cross domain calls
-         $httpProvider.defaults.useXDomain = true;
-         //Remove the header used to identify ajax call  that would prevent CORS from working
-         delete $httpProvider.defaults.headers.common['X-Requested-With'];
-     })
+    // .config(function($httpProvider) {
+    //      //Enable cross domain calls
+    //      $httpProvider.defaults.useXDomain = true;
+    //      //Remove the header used to identify ajax call  that would prevent CORS from working
+    //      delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    //  })
      .run(run)
   ;
 
